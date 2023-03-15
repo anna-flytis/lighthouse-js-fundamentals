0dbe58b39c6c5395cd33b27d2da24c81030fe84f
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-03-14 19:21:18 -0600

    array ice-cream

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+// Do not modify these first two lines
+
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+let newAr = [];
+newAr = iceCreamFlavours.slice();
+newAr.push('root beer');
+console.log(newAr);
+
+console.log(newAr[0]);
+console.log(newAr[5]);
+console.log(newAr.length);
+
